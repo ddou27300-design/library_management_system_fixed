5dc728f7a9773e20bb1b557fc2ee134f5ee130bb
Thorng Dy <diigenz4141@gmail.com>
2026-05-28 21:02:51 +0700

    Fix: force secure session cookies by default

--- config/session.php
+++ config/session.php
@@ -169,7 +169,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE'),
+    'secure' => env('SESSION_SECURE_COOKIE', true), // <-- បន្ថែម , true នៅត្រង់នេះ
 
     /*
     |--------------------------------------------------------------------------
